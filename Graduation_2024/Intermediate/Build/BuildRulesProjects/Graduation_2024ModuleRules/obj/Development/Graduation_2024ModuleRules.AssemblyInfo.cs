e5d3ca94998574321964ec9f1be49ebf906d9933
yuanxiaoxy <2891944482@qq.com>
2024-11-14 18:31:12 +0800

    Some Changes

--- Graduation_2024/Intermediate/Build/BuildRulesProjects/Graduation_2024ModuleRules/obj/Development/Graduation_2024ModuleRules.AssemblyInfo.cs
+++ Graduation_2024/Intermediate/Build/BuildRulesProjects/Graduation_2024ModuleRules/obj/Development/Graduation_2024ModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graduation_2024ModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5516a34adad6ae306dc2f0fd195668244dee189b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a3cf2f192f91ced9bccac8dab1dbd119e79f55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graduation_2024ModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graduation_2024ModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
